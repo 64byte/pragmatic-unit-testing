0bdfd89ac999786be95c9c0e51af6f2423b0b493
16byte <8byte@naver.com>
2021-01-11 01:19:42 +0900

    assertEqual commit

--- chapter-01/src/test/ScoreCollectionTest.kt
+++ chapter-01/src/test/ScoreCollectionTest.kt
@@ -1,12 +1,21 @@
 package test
 
+import iloveyouboss.main.ScoreCollection
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
+import org.junit.*;
+import kotlin.test.assertEquals
 
 public class ScoreCollectionTest {
     @Test
     public fun test() {
-        fail("Not yet implemented")
+        var collection: ScoreCollection = ScoreCollection();
+        collection.add { 5 };
+        collection.add { 7 };
+
+        var actualResult: Int = collection.arithemticMean();
+
+        assertEquals(6, actualResult);
+
     }
 
 }
